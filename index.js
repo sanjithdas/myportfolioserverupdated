7fc2b98441f57fa42a1e11c38a5fe5f0678b5f54
sanjithdas <sanjith.das@gmail.com>
2021-06-12 18:46:53 +1000

    updated

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 const express = require('express');
 require('dotenv').config();
 
+const cors = require('cors');
+
 // Init express
 const app = express();
+
+app.use(cors());
 
 // Import central routes
 const routes = require('./routes/routes');
